822e4c2931ec7d90111dc307e29c615fad0b3ca2
Alexander Phillips <alexphillipsdev@gmail.com>
2026-02-11 22:35:14 -0600

    init

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,7 +1,35 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-    plugins: [react()],
-});
+// https://vitejs.dev/config/
+export default ({ mode }) => {
+    process.env = { ...process.env, ...loadEnv(mode, process.cwd()) };
+
+    return defineConfig({
+        build: {
+            rollupOptions: {
+                external: [
+                    "**/*.test.ts",
+                    "**/*.spec.ts",
+                    "**/*.ignore.*",
+                    "*.ignore.*/**",
+                ],
+            },
+        },
+        plugins: [
+            react(), // <-- just the default plugin
+        ],
+        define: {
+            "window.l": "console.log",
+            l: "console.log",
+        },
+        server: {
+            proxy: {
+                "/api": {
+                    target: process.env.VITE_BASE_URL,
+                    changeOrigin: false,
+                },
+            },
+        },
+    });
+};
